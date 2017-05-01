ce13ec2d69168cee2677a2d22f0ca6ac9c84beae
SoloVeniaASaludar <daysbizarre@gmail.com>
2017-05-01 21:12:56 +0200

    +

--- lg_smart_tv/__init__.py
+++ lg_smart_tv/__init__.py
@@ -11,44 +11,67 @@ import httplib as http
 import xml.etree.ElementTree as etree
 
 from adapt.intent import IntentBuilder
-from mycroft.skills.multi_thread_skill import MultiThreadSkill, SkillSession
+from mycroft.skills.core import MycroftSkill
 from mycroft.util.log import getLogger
 
 logger = getLogger(__name__)
 
 headers = {"Content-Type": "application/atom+xml"}
 
-class LgSmartTvSession(SkillSession):
+class LgSmartTvSkill(MycroftSkill):
 
-    def __init__(self, id, skill):
-        super(LgSmartTvSession, self).__init__(id, skill)
-        self.ip_address = self.skill.config.get('ip_address', '192.168.1.36')
-        self.conn = http.HTTPConnection(self.ip_address, port=8080)
+    def __init__(self):
+        super(LgSmartTvSkill, self).__init__("LgSmartTvSkill")
+        self.conn = None
         self.session_id = None
+
+    def initialize(self):
+        ip_address = self.config.get('ip_address')
+        if not ip_address:
+            self.speak_dialog('configure_ip_address')
+            return
+        self.conn = http.HTTPConnection(ip_address, port=8080)
+
+        intent = IntentBuilder("ChangeTvChannelIntent").require(
+            "ChangeTvChannel").build()
+        self.register_intent(intent, self.change_tv_channel)
+
+    def send_get(self, urn):
+        self.conn.request("GET", urn, None, headers=headers)
+        resp = self.conn.getresponse().read()
+        logger.debug(resp)
+        return etree.XML(resp)
+
+    def send_post(self, urn, xml=None):
+        self.conn.request("POST", urn, xml, headers=headers)
+        resp = self.conn.getresponse().read()
+        logger.debug(resp)
+        return etree.XML(resp)
 
     def display_pairing_key(self):
         xml = '<?xml version="1.0" encoding="utf-8"?><auth><type>AuthKeyReq</type></auth>'
-        self.conn.request("POST", "/roap/api/auth", xml, headers=headers)
-        self.conn.getresponse()
+        self.send_post("/roap/api/auth", xml)
 
     def open_session(self):
-        pairing_key = self.skill.config.get('pairing_key')
+        pairing_key = self.config.get('pairing_key')
         if not pairing_key:
             self.display_pairing_key()
             self.speak_dialog('configure_pairing_key')
             return
 
         xml = '<?xml version="1.0" encoding="utf-8"?><auth><type>AuthReq</type><value>%s</value></auth>'%pairing_key
-        self.conn.request("POST", "/roap/api/auth", xml, headers=headers)
-        response = self.conn.getresponse().read()
-        tree = etree.XML(response)
+        tree = self.send_post("/roap/api/auth", xml)
         self.session_id = tree.find('session').text
         logger.debug("session_id=%s", self.session_id)
 
-    def send_command(self, cmnd):
+    def send_handle_key_input(self, cmnd):
         xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?><command><session>%s</session><type>HandleKeyInput</type><value>%s</value></command>"%(self.session_id,cmnd)
-        self.conn.request("POST", "/roap/api/command", xml, headers=headers)
-        response = self.conn.getresponse().read()
+        self.send_post("/roap/api/command", xml)
+
+    def query_volume_info(self):
+        tree = self.send_get("/udap/api/data?target=volume_info")
+        level = tree.find('data/level').text
+        logger.debug("level=%s",level)
 
     #
     # execution of command "pon canal ..."
@@ -58,21 +81,9 @@ class LgSmartTvSession(SkillSession):
         if not self.session_id:
             self.open_session()
         if self.session_id:
-            self.send_command(6)
-
-class LgSmartTvSkill(MultiThreadSkill):
-    def __init__(self):
-        # call super with the class used for the sessions
-        super(LgSmartTvSkill, self).__init__(
-                   "LgSmartTvSkill", LgSmartTvSession)
-
-    def initialize(self):
-        super(LgSmartTvSkill,self).initialize()
-
-        # register intents in the usual way
-        intent = IntentBuilder("ChangeTvChannelIntent").require(
-            "ChangeTvChannel").build()
-        self.register_intent(intent, LgSmartTvSession.change_tv_channel)
+            self.send_handle_key_input(6)
+            self.query_volume_info()
 
 def create_skill():
     return LgSmartTvSkill()
+
